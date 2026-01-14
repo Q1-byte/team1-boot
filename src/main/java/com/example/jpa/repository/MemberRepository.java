8ac10dbc6998d61c240a7243285557b7e7e6ced2
Q1-byte <gyuwon8440@gmail.com>
2026-01-14 10:33:03 +0900

    init spring boot project

---
+++ src/main/java/com/example/jpa/repository/MemberRepository.java
@@ -0,0 +1,30 @@
+package com.example.jpa.repository;
+
+import com.example.jpa.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+        Member findMemberByPhone(String phone);
+
+        List<Member> findByAge(int age);
+
+        Member findByNameAndAddress(String name, String address);
+
+        List<Member> findByAgeGreaterThanEqual(int age);
+
+        List<Member> findByAddressLike(String address);
+
+        List<Member> findByAddressOrderByAgeAsc(String address);
+
+        @Query("select m from Member m where m.address like concat('%', :address, '%') order by m.age asc")
+        List<Member> findByAddressAgeAsc(@Param("address") String address);
+
+        @Query("select m from Member m where m.age >= :age order by m.age desc")
+        List<Member> findByAgeOrderByDesc(@Param("age") int age);
+}
---
+++ src/test/java/com/example/jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,87 @@
+package com.example.jpa.repository;
+
+import com.example.jpa.domain.Member;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Log4j2
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    //추가
+    @Test
+    public void insertTest(){
+
+        Member member = Member.builder()
+                .name("길동")
+                .age(2)
+                .phone("111")
+                .address("강원도")
+                .build();
+
+    memberRepository.save(member);
+    }
+
+    //수정
+    @Test
+    public void updateTest() {
+        Optional<Member> optMember = memberRepository.findById(3);
+        Member member = optMember.get();
+
+        member.setName("뽀양");
+        member.setAge(3);
+        member.setAddress("카츠동");
+
+        memberRepository.save(member);
+    }
+
+    //삭제
+    @Test
+    public void deleteTest(){
+
+        memberRepository.deleteById(2);
+    }
+
+    //전체데이타 조회
+    @Test
+    public  void  selectAll(){
+        List<Member> memberList = memberRepository.findAll();
+
+        memberList.forEach(member -> log.info(member));
+    }
+
+    //조회
+    @Test
+    public void selectTest(){
+
+        List<Member> memberList =
+                memberRepository.findByAgeGreaterThanEqual(13);
+
+        memberList.forEach(member -> log.info(member));
+        log.info(memberList);
+    }
+
+    @Test
+    public void likeTest(){
+        List<Member> memberList = memberRepository.findByAddressLike("인천%");
+        memberList.forEach(member -> log.info(member));
+    }
+
+    @Test
+    public void orderAge(){
+        // List<Member> memberList = memberRepository.findByAddressOrderByAgeAsc("인천");
+        List<Member> memberList = memberRepository.findByAgeOrderByDesc(13);
+        memberList.forEach(member -> log.info(member));
+    }
+
+}
